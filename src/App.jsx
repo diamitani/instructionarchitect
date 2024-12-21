cee4323788626634f5516d0823a5e99f4c592af1
Patrick Diamitani <patrick.diamitani@gmail.com>
2024-12-20 19:45:33 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AIChat from "./components/AIChat";
+
+const App = () => {
+  return (
+    <div className="h-screen flex flex-col">
+      <AIChat />
+    </div>
+  );
+};
+
+export default App;
